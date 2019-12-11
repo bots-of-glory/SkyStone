c1c918870de5d4517d3d761333a627a102be66ab
alexander-smawesome <51275303+alexander-smawesome@users.noreply.github.com>
2019-12-11 08:36:15 -0600

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake_Test.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp (name="Intake_Test", group="testOp")
+public class Intake_Test extends LinearOpMode {
+    DcMotor intake1;
+    DcMotor intake2;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //declare motors
+        intake1 = hardwareMap.dcMotor.get("intake1");
+        intake2 = hardwareMap.dcMotor.get("intake2");
+
+        //declare motor directions
+        intake1.setDirection(DcMotor.Direction.REVERSE);
+        intake2.setDirection(DcMotor.Direction.FORWARD);
+
+        intake1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            intake1.setPower(1 * gamepad1.right_stick_y);
+            intake2.setPower(1 * gamepad1.right_stick_y);
+
+        }
+        // AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Right,2600,0.6,true, telemetry);
+        // AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight, AutonomousCommon.StrafeDirection.Backward,2600,0.6,true, telemetry);
+        // AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Left,2600,0.6,true, telemetry);
+        //AutonomousCommon.macanumBox(frontLeft,rearLeft,frontRight,rearRight,1000,.3,true, telemetry);
+
+
+    }
+
+}
