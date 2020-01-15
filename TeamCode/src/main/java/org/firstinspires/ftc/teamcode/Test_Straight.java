1c3ea8e098ab6a910899f1c876e69c99e441d9a5
Chris.Hettinger <chrishettinger1@gmail.com>
2020-01-14 20:05:55 -0600

    tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Straight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Straight.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp (name="Test_Straight", group="testOp")
 public class Test_Straight extends SkystoneBase {
     @Override
@@ -7,10 +9,81 @@ public class Test_Straight extends SkystoneBase {
         this.playSide = AutonomousCommon.PlayfieldSide.Blue;
         initMotors();
         waitForStart();
-        int calibrationTest = (AutonomousCommon.convertInchesToPosition(10, false));
-        int strafeTest = (AutonomousCommon.convertInchesToPosition(10,true));
-        AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Forward,10,.6,opModeIsActive(), telemetry);
-        AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Right,10,.6,opModeIsActive(),telemetry);
+        //int calibrationTest = (AutonomousCommon.convertInchesToPosition(10, false));
+        //int strafeTest = (AutonomousCommon.convertInchesToPosition(10,true));
+        //AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Forward,10,.6,opModeIsActive(), telemetry);
+        //AutonomousCommon.macanumMovement(frontLeft,rearLeft,frontRight,rearRight,AutonomousCommon.StrafeDirection.Right,10,.6,opModeIsActive(),telemetry);
+        //rearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //rearRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        int target = 1000;
+        int startPosition = frontLeft.getCurrentPosition();
+        frontLeft.setPower(.6);
+        frontLeft.setTargetPosition(startPosition+target);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (frontLeft.isBusy() ) {
+            telemetry.addData("Status","Busy");
+        }
+        else{
+            telemetry.addData("Status","Free");
+            sleep(500);
+            target=-target;
+            frontLeft.setTargetPosition(startPosition+target);
+        }
+        telemetry.update();
+        frontLeft.setPower(0);
+
+        target = 1000;
+        startPosition = frontRight.getCurrentPosition();
+        frontRight.setPower(.6);
+        frontRight.setTargetPosition(startPosition+1000);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (frontRight.isBusy() ) {
+            telemetry.addData("Status","Busy");
+        }
+        else{
+            telemetry.addData("Status","Free");
+            sleep(500);
+            target=-target;
+            frontRight.setTargetPosition(startPosition+target);
+        }
+        telemetry.update();
+        frontRight.setPower(0);
+
+        target = 1000;
+        startPosition = rearLeft.getCurrentPosition();
+        rearLeft.setPower(.6);
+        rearLeft.setTargetPosition(startPosition+1000);
+        rearLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (rearLeft.isBusy() ) {
+            telemetry.addData("Status","Busy");
+        }
+        else{
+            telemetry.addData("Status","Free");
+            sleep(500);
+            target=-target;
+            rearLeft.setTargetPosition(startPosition+target);
+        }
+        telemetry.update();
+        rearLeft.setPower(0);
+
+        target = 1000;
+        startPosition = rearRight.getCurrentPosition();
+        rearRight.setPower(.6);
+        rearRight.setTargetPosition(startPosition+1000);
+        rearRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (rearRight.isBusy() ) {
+            telemetry.addData("Status","Busy");
+        }
+        else{
+            telemetry.addData("Status","Free");
+            sleep(500);
+            target=-target;
+            rearRight.setTargetPosition(startPosition+target);
+        }
+        telemetry.update();
+        rearRight.setPower(0);
 
     }
 
